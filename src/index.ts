9a25b51cc28e3b5aecd57ccaaebfb145422e6a9f
Roger Chappel <roger@users.noreply.github.com>
2026-05-02 15:52:32 +1000

    feat: expose library api

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { discoverRepos } from './discover.js';
+export { collectGit } from './git.js';
+export { loadGitHubFixtures, attachGitHubMetadata } from './github-fixtures.js';
+export { render } from './render.js';
+export { scan } from './scan.js';
+export { sortRepos } from './sort.js';
+export type { GitHubMetadata, RenderFormat, RepoBeacon, ScanOptions, SortMode } from './types.js';
